73ddd3d5b98b0376bbb5d53a442af2e28ba19338
Anna <you@example.com>
2022-07-29 18:51:19 +0300

    создание классов

--- hw_02_oop/src/Figure.py
+++ hw_02_oop/src/Figure.py
@@ -1,8 +1,10 @@
-class Figure():
-	def add_area(self, figure):
-		if (figure.__class__ == "<class 'Square.Square'>" or
-			figure.__class__ == "<class 'Triangle.Triangle'>" or
-			figure.__class__ == "<class 'Rectangle.Rectangle'>" or
-			figure.__class__ == "<class 'Circle.Circle'>" or)
-			return self.area + figure.area
-		raise ValueError
+class Figure:
+    area = 0
+
+    def add_area(self, figure):
+        if (figure.__class__ == "<class 'Square.Square'>" or
+                figure.__class__ == "<class 'Triangle.Triangle'>" or
+                figure.__class__ == "<class 'Rectangle.Rectangle'>" or
+                figure.__class__ == "<class 'Circle.Circle'>"):
+            return self.area + figure.area
+        raise ValueError
